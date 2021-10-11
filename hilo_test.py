625416156ef7fdbab72cd15e61dacce1b81a6ebb
Ahanna Ndujiuba <92158853+a-ndujiuba@users.noreply.github.com>
2021-10-11 17:11:23 +0100

    Update hilo_test.py

--- hilo_test.py
+++ hilo_test.py
@@ -1,47 +1,29 @@
 LOW = 1
 HIGH = 1000
 
-# print("Please think of a number between 1 and 1000")
-# input("Press ENTER to start")
+start = input("Press ENTER to Start")
+print(f"Think of a number between {LOW} and {HIGH}")
+next_step = input("Press ENTER when you have thought of a number")
 
 
-def guess_binary(answer, low, high):
+def guess_binary(low, high):
     guesses = 1
     while True:
-        # print("\tGuessing in the range {} to {}".format(low, high))
         guess = low + (high - low) // 2
-        # high_low = input("My guess is {}. Should I guess higher or lower? Enter h or l, or c if my guess was correct."
-        #                  .format(guess)).casefold()
+        print(f"Is {guess} the number you were thinking of?")
+        high_low = input('Type "l" to guess lower, "h" to guess higher, and "c" if I was correct ')
 
-        # if high_low == "h":
-        if guess < answer:
-            # I have to guess higher.  The low end of the range becomes 1 greater than the guess.
+        if high_low == 'h':
             low = guess + 1
-        # elif high_low == "l":
-        elif guess > answer:
-            # I have to guess lower.  The high end of the range becomes 1 less than the guess.
+            guesses += 1
+        elif high_low == 'l':
             high = guess - 1
-        # elif high_low == "c":
-        elif guess == answer:
-            # print("I got it in {} guesses!".format(guesses))
-            # break
-            return guesses
-        # else:
-        #     print("Please enter h, l or c")
+            guesses += 1
+        elif high_low == 'c':
+            print("I got it in {} guesses!".format(guesses))
+            break
 
-        guesses += 1
+    return guesses
 
 
-correct_count = 0
-max_guesses = 0
-for number in range(LOW, HIGH + 1):
-    number_of_guesses = guess_binary(number, LOW, HIGH)
-    print("{} guessed in {}".format(number, number_of_guesses))
-
-    if number_of_guesses > max_guesses:
-        max_guesses, correct_count = number_of_guesses, 1
-    elif number_of_guesses == max_guesses:
-        correct_count += 1
-
-print("I guessed without being told {} times. Max {} guesses."
-      .format(correct_count, max_guesses))
+guess_binary(LOW, HIGH)
